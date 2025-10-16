830646db8548bb97e4eaf47018b0078a99b6e3d8
Shivaji Puri <shivaji9961@gmail.com>
2025-10-16 07:50:19 +0530

    change port numbers

--- energy_project/Project_files/config.py
+++ energy_project/Project_files/config.py
@@ -1,6 +1,6 @@
 # MODEL_FILE_PATH = r'Linear_Model.pkl'
 # JSON_FILE_PATH = r'project_data.json'
 
-PORT_NUMBER = 5005
+PORT_NUMBER = 8080
 MODEL_FILE_PATH = r'Project_files\Linear_Model.pkl'
 JSON_FILE_PATH = r'Project_files\project_data.json'
